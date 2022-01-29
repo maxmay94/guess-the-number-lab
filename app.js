1dac21da5bc778e4b457a3ae9d689b4b90a63461
maxmay <maxmay94@gmail.com>
2022-01-29 11:40:28 -0500

    Bug fixes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+
+  /// PLAY
   play: function() {
     //create random number
     this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
@@ -11,43 +13,74 @@ const game = {
     let guess
     // get into guess loop
     do {
-      guess = this.getGuess() 
-      this.prevGuesses.push(guess) // add guess to prevGuesses
-      this.render(guess, this.secretNum);
+      guess = this.getGuess() // invoke getGuess() return result to guess
       console.log(`user input was ${guess}`) // logs most recent guess
-    } while(parseInt(guess) !== parseInt(this.secretNum)) //exits loop when user correctly guesses secretNum
+    } while(this.render(guess, this.secretNum) === false)
     
     console.log("You got it!")
     return
   },
+
+
+  /// GET GUESS
   getGuess: function() {
-    // promt user to make guess, save input as guess
+    //console.log('top of getGuess')
+
+    // promt user to make guess between smallestNum and biggestNum, save input as guess
     let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `)
-    // if guess is a number between smallestNum and Biggest Num
+    // if guess is a NUMBER between smallestNum and Biggest Num
     if(Number.isInteger(parseInt(guess)) && guess >= this.smallestNum && guess <= this.biggestNum){
-      // add guess to prevGuesses
-      return guess
+      this.prevGuesses.push(guess) // add guess to prevGuesses
     } else if(guess === 'quit') {
-      console.log('asdf')
+      return 
     } else {
-      // if it fails the if statement, try again
-      this.getGuess()
+      guess = 'Invalid Guess'
     }
+    return guess
   },
+
+  /// RENDER
   render: function(usr, rnd) {
+    let solve = false
+
+    //If usr input = random number AND there has been more than one guess
     if(parseInt(usr) === parseInt(rnd) && this.prevGuesses.length > 1) { // congrats message
       window.alert(`Congrats! You guessed the number in ${this.prevGuesses.length} tries!`)
+
+      solve = true
+
     } else if(parseInt(usr) === parseInt(rnd)) { // Check if user solved in 1 try and give super secret special message
-      window.alert(`Congrats! You guessed the number in ${this.prevGuesses.length} try...
-      You're not cheating are you?`) 
-    } else if(parseInt(usr) > rnd) { //guess too high
+      window.alert(`Congrats! You guessed the number in ${this.prevGuesses.length} try...You're not cheating are you?`)
+
+      solve = true
+
+    } else if(parseInt(usr) > rnd && Number.isInteger(parseInt(usr))) { //guess too high, triple check that usr is a number
       window.alert(`Your guess is too High
       previous guesses: ${this.prevGuesses.toString()}`) 
-    } else { // guess too low
+
+      this.biggestNum = usr // set biggestNum to user guess
+      solve = false
+
+    } else if(parseInt(usr) < rnd && Number.isInteger(parseInt(usr))){ // guess too low, triple check usr is actually a number
       window.alert(`Your guess is too Low
       previous guesses: ${this.prevGuesses.toString()}`)
+
+      this.smallestNum = usr // set smallestNum to user guess
+      solve = false
+
+    } else if(usr === 'Invalid Guess') {
+      window.alert(`Nice try bozo`)
+      console.log(`usr was equal to: ${usr} & previous guesses are: ${this.prevGuesses}`)
+      solve = false
+    } else {
+      window.alert('Something has gone terribly wrong...')
+      return
     }
+    return solve
   },
+
+
+  /// RESIZE
   resize: function(){
     
   }
