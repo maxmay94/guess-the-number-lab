88b3f6c3c5746557a84c7640c834d596f971621d
maxmay <maxmay94@gmail.com>
2022-01-28 19:00:14 -0500

    Add render feature

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+  play: function() {
+    //create random number
+    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    console.log(this.secretNum)
+    let guess
+    // get into guess loop
+    do {
+      guess = this.getGuess() 
+      this.prevGuesses.push(guess) // add guess to prevGuesses
+      this.render(guess, this.secretNum);
+      console.log(`user input was ${guess}`) // logs most recent guess
+    } while(parseInt(guess) !== parseInt(this.secretNum)) //exits loop when user correctly guesses secretNum
+    
+    console.log("You got it!")
+  },
   getGuess: function() {
     // promt user to make guess, save input as guess
     let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `)
     // if guess is a number between smallestNum and Biggest Num
     if(Number.isInteger(parseInt(guess)) && guess >= this.smallestNum && guess <= this.biggestNum){
       // add guess to prevGuesses
-      this.prevGuesses.push(guess)
+      return guess
     } else if(guess === 'quit') {
       console.log('asdf')
     } else {
@@ -18,18 +33,16 @@ const game = {
       this.getGuess()
     }
   },
-  play: function() {
-    //create random number
-    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    console.log(this.secretNum)
-
-    do {
-      this.getGuess()
-      console.log(`user input was ${this.prevGuesses[this.prevGuesses.length - 1]}`) // logs most recent guess
-    } while(parseInt(this.prevGuesses[this.prevGuesses.length - 1]) !== parseInt(this.secretNum)) //exits loop when user correctly guesses secretNum
-
-    console.log("You got it!")
-
+  render: function(usr, rnd) {
+    if(parseInt(usr) === parseInt(rnd)) {
+      window.alert(`Congrats! You guessed the number in ${this.prevGuesses.length} tries!`)
+    } else if(parseInt(usr) > rnd) {
+      window.alert(`Your guess is too High
+      previous guesses: ${this.prevGuesses.toString()}`)
+    } else {
+      window.alert(`Your guess is too Low
+      previous guesses: ${this.prevGuesses.toString()}`)
+    }
   }
 }
 
