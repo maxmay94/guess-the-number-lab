9f09379380410cd3447bf3f0c6e1118d9ca8ca3b
maxmay <maxmay94@gmail.com>
2022-01-30 10:56:21 -0500

    final version, all bonus objectives complete

--- app.js
+++ app.js
@@ -7,30 +7,30 @@ const game = {
 
   /// PLAY
   play: function() {
-    //create random number
+
+    // Set new Range
+    this.resize()
+
+    // create random number
     this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    console.log(this.secretNum)
+
     let guess
     // get into guess loop
     do {
       guess = this.getGuess() // invoke getGuess() return result to guess
-      console.log(`user input was ${guess}`) // logs most recent guess
-    } while(this.render(guess, this.secretNum) === false)
-    
-    console.log("You got it!")
+    } while(this.render(guess) === false)
+
     return
   },
 
 
   /// GET GUESS
   getGuess: function() {
-    //console.log('top of getGuess')
-
     // promt user to make guess between smallestNum and biggestNum, save input as guess
     let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `)
     // if guess is a NUMBER between smallestNum and Biggest Num
     if(Number.isInteger(parseInt(guess)) && guess >= this.smallestNum && guess <= this.biggestNum){
-      this.prevGuesses.push(guess) // add guess to prevGuesses
+      this.prevGuesses.push(parseInt(guess)) // add guess to prevGuesses
     } else if(guess === 'quit') {
       return 
     } else {
@@ -40,37 +40,30 @@ const game = {
   },
 
   /// RENDER
-  render: function(usr, rnd) {
+  render: function(usr) {
     let solve = false
 
     //If usr input = random number AND there has been more than one guess
-    if(parseInt(usr) === parseInt(rnd) && this.prevGuesses.length > 1) { // congrats message
+    if(parseInt(usr) === this.secretNum && this.prevGuesses.length > 1) { // congrats message
       window.alert(`Congrats! You guessed the number in ${this.prevGuesses.length} tries!`)
-
       solve = true
-
-    } else if(parseInt(usr) === parseInt(rnd)) { // Check if user solved in 1 try and give super secret special message
+    } else if(parseInt(usr) === this.secretNum) { // Check if user solved in 1 try and give super secret special message
       window.alert(`Congrats! You guessed the number in ${this.prevGuesses.length} try...You're not cheating are you?`)
-
       solve = true
-
-    } else if(parseInt(usr) > rnd && Number.isInteger(parseInt(usr))) { //guess too high, triple check that usr is a number
+    } else if(parseInt(usr) > this.secretNum && Number.isInteger(parseInt(usr))) { //guess too high, triple check that usr is a number
       window.alert(`Your guess is too High
       previous guesses: ${this.prevGuesses.toString()}`) 
 
       this.biggestNum = usr // set biggestNum to user guess
       solve = false
-
-    } else if(parseInt(usr) < rnd && Number.isInteger(parseInt(usr))){ // guess too low, triple check usr is actually a number
+    } else if(parseInt(usr) < this.secretNum && Number.isInteger(parseInt(usr))){ // guess too low, triple check usr is actually a number
       window.alert(`Your guess is too Low
       previous guesses: ${this.prevGuesses.toString()}`)
 
       this.smallestNum = usr // set smallestNum to user guess
       solve = false
-
     } else if(usr === 'Invalid Guess') {
       window.alert(`Nice try bozo`)
-      console.log(`usr was equal to: ${usr} & previous guesses are: ${this.prevGuesses}`)
       solve = false
     } else {
       window.alert('Something has gone terribly wrong...')
@@ -82,7 +75,30 @@ const game = {
 
   /// RESIZE
   resize: function(){
-    
+    let newRange
+    let small
+    let big
+    newRange = window.prompt('Enter your desired range, please use two numbers seperated by a space.')
+
+    // spilt user input into array, convert from strings into numbers
+    newRange = newRange.split(' ')
+    small = newRange[0]
+    big = newRange[1]
+    small = parseInt(small)
+    big = parseInt(big)
+
+    // IF user didn't enter 2 thing or either of them isn't a number
+    if(newRange.length !== 2 || !Number.isInteger(big) || !Number.isInteger(small)) {
+      window.alert(`You're doing something wrong...`)
+      this.resize()
+    } else if(small >= big) { //is the second number was bigger than the first, swap them 
+      this.smallestNum = big
+      this.biggestNum = small
+    }
+    else {
+      this.smallestNum = small
+      this.biggestNum= big
+    }
   }
 }
 
